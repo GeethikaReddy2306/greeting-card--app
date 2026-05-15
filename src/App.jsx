cae7090ecb8e22aab4f77dd7e70a00ace4f0055c
geethika <geethikareddy051@gmail.com>
2026-05-15 16:53:30 +0530

    completed the card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import "./App.css";
 export default function App() {
+    const students = [
+   {
+      id: 1,
+      name: "Geetha",
+      college: "Vignan University",
+      skill: "React"
+   },
+
+   {
+      id: 2,
+      name: "Ram",
+      college: "ABC Engineering College",
+      skill: "Java"
+   },
+
+   {
+      id: 3,
+      name: "Sam",
+      college: "XYZ Institute of Technology",
+      skill: "Node.js"
+   },
+
+   {
+      id: 4,
+      name: "Ravi",
+      college: "PQR College",
+      skill: "Python"
+   },
+
+   {
+      id: 5,
+      name: "Anjali",
+      college: "SRM University",
+      skill: "UI/UX Design"
+   },
+
+   {
+      id: 6,
+      name: "Kiran",
+      college: "JNTU",
+      skill: "Machine Learning"
+   }
+];
   return (
     <div>
       <Navbar/>
+  
+      {
+        students.map((student)=>(
+          <Card name={student.name} college={student.college} skills={student.skill}/>
+        ))
+      }
+   
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,13 @@
-import "./App.css";
+
+export default function Card({name,college,skills}) {
+
+  return (
+    <div id="layout">
+    <div id="card">
+      <h2>Name : {name}</h2>
+      <p>College : {college}</p>
+      <p>Skills : {skills}</p>
+    </div>
+    </div>
+  )
+}
